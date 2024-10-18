69b06885e11001955b196904d872a3a0990e1784
LeanGomez <leanngomezzz05@gmail.com>
2024-10-18 00:16:26 -0300

    Modelo Parcial 3

---
+++ src/test/Main.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package test;
+
+/**
+ *
+ * @author leand
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1-1");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-2");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-3");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-4");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-5");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-6");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-7");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+        //////////////////////////////////////////////////////////////////
+        
+        System.out.println("\n1-9");
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+           
+        
+    }
+    
+    
+    
+    
+    
+}
